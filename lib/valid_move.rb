018b614ac9371555f1914df43550e737f61facb4
Jacob Borga <jdborga@gmail.com>
2021-03-04 03:09:43 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@ def valid_move?(board, index)
   if index > 8 || index < 0
     return false
   end
-  if position_taken(board, index)
+  if position_taken?(board, index)
     return true
   end
   return false
